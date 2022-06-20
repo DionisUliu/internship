6db44ea5398f3c1fe171043e869992007b01e84f
Dionis <dioniuliu@gmail.com>
2022-06-20 16:44:28 +0200

    Configuring react routes

--- react/online-bookstore-app/src/App.tsx
+++ react/online-bookstore-app/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import LogIn from "./components/routes/auth/logIn/logIn";
 import "antd/dist/antd.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <>
-      <LogIn />
+      <Routes>
+        <Route path="/auth/login" element={<LogIn />} />
+        <Route path="*" element={<Navigate to="/auth/login" />} />
+      </Routes>
     </>
   );
 }
